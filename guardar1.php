854bac2b94ee47852e6aab7042858791110631a6
DavidFGDH <32119775+DavidFGDH@users.noreply.github.com>
2018-11-12 03:19:58 +0000

    Update and rename guardar.php to guardar1.php

--- guardar1.php
+++ guardar1.php
@@ -1,41 +1,82 @@
 <?php
+
 $db_host="dabda-mysqldbserver.mysql.database.azure.com";
+
 $db_user="davidmaster@dabda-mysqldbserver";
+
 $db_password="Redderedes.123";
-$db_name="pagina";
-$db_table_name="pags";
+
+$db_name="forumulario";
+
+$db_table_name="form";
+
    $db_connection = mysql_connect($db_host, $db_user, $db_password);
 
+
+
 if (!$db_connection) {
+
 	die('No se ha podido conectar a la base de datos');
+
 }
-$subs_name = utf8_decode($_POST['nombre']);
-$subs_last = utf8_decode($_POST['apellido']);
-$subs_email = utf8_decode($_POST['email']);
+
+$subs_numeroC = utf8_decode($_POST['NDP']);
+
+$subs_PrimAp = utf8_decode($_POST['PA']);
+$subs_Segap = utf8_decode($_POST['PA2']);
+$subs_Nombr = utf8_decode($_POST['NO']);
+$subs_email = utf8_decode($_POST['sel2']);
+
+
 
 $resultado=mysql_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'", $db_connection);
 
+
+
 if (mysql_num_rows($resultado)>0)
+
 {
+
+
 
 header('Location: Fail.html');
 
+
+
 } else {
+
 	
-	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`Nombre` , `Apellido` , `Email`) VALUES ("' . $subs_name . '", "' . $subs_last . '", "' . $subs_email . '")';
+
+	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`NumCon` , `Primer_Apellido` , `Segundo_Apellido` , `Nombre` , `Sexo`) VALUES ("' . $subs_numeroC . '", "' . $subs_PrimAp . '", "' . $subs_Segap . '","' . $subs_Nombr . '","' . $subs_email . '")';
+
+
 
 mysql_select_db($db_name, $db_connection);
+
 $retry_value = mysql_query($insert_value, $db_connection);
 
+
+
 if (!$retry_value) {
+
    die('Error: ' . mysql_error());
-}
-	
-header('Location: Success.html');
 
 }
+
+	
+
+header('Location: Success.html');
+
+
+
+}
+
+
 
 mysql_close($db_connection);
 
+
+
 		
+
 ?>
